b723493850a3460614c7b9d4d1444deddce0e50a
Alexander Römer <alex@roemer.design>
2020-10-10 18:50:19 +0200

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Alexander Römer on 10.10.20.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
